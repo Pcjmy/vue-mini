86b3d338f971467d0402bbfd2caf1180e87b5b68
Pcjmy <1716045191@qq.com>
2023-03-23 22:47:00 +0800

    feat: 配置路径映射

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # README
 
 vue-mini
+
+配置路径映射
+
+```shell
+设置快捷导入
+"baseUrl": ".",
+"paths": {
+  "@vue/*": ["packages/*/src"]
+}
+```
--- packages/vue/dist/vue.js
+++ packages/vue/dist/vue.js
@@ -1,7 +1,12 @@
 (function () {
 	'use strict';
 
-	console.log('hello');
+	/**
+	 * 判断是否为一个数组
+	 */
+	var isArray = Array.isArray;
+
+	console.log(isArray([]));
 
 })();
 //# sourceMappingURL=vue.js.map
