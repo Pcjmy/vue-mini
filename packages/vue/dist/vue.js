579a15c425ca47ff08ed1cf8727310188a426d8d
Pcjmy <1716045191@qq.com>
2023-03-26 14:58:29 +0800

    feat: 收集依赖和触发依赖

--- packages/vue/dist/vue.js
+++ packages/vue/dist/vue.js
@@ -1,7 +1,44 @@
 var Vue = (function (exports) {
     'use strict';
 
-    var mutableHandlers = {};
+    /**
+     * 收集依赖
+     * @param target
+     * @param key
+     */
+    function track(target, key) {
+        console.log('track: 收集依赖');
+    }
+    /**
+     * 触发依赖
+     * @param target
+     * @param key
+     * @param newValue
+     */
+    function trigger(target, key, newValue) {
+        console.log('trigger: 触发依赖');
+    }
+
+    var get = createGetter();
+    function createGetter() {
+        return function get(target, key, receiver) {
+            var res = Reflect.get(target, key, receiver);
+            track();
+            return res;
+        };
+    }
+    var set = createSetter();
+    function createSetter() {
+        return function set(target, key, value, receiver) {
+            var result = Reflect.set(target, key, value, receiver);
+            trigger();
+            return result;
+        };
+    }
+    var mutableHandlers = {
+        get: get,
+        set: set
+    };
 
     var reactiveMap = new WeakMap();
     function reactive(target) {
